7ae9eb855aa09b537100627c74a5687e3f21ee4a
forteleaf <forteleaf@live.com>
2016-06-26 22:35:26 +0900

    haha

--- getPacktpubBook/login_cookiejar.go
+++ getPacktpubBook/login_cookiejar.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 const (
 	packtPubBaseURL string = "https://www.packtpub.com"
@@ -8,5 +12,20 @@ const (
 )
 
 func main() {
-	cj := http.CookieJar.SetCookies(u, cookies)
+	data := url.Values{"email": "forteleaf@gmail.com",
+		"password": "shuria40",
+		"op":       "Login",
+	}
+
+	res, _ := http.PostForm(packtPubBaseURL, data)
+
+	if res.StatusCode == 200 {
+		cookies := res.Cookies()
+		for k, v := range cookies {
+			fmt.Println(k, v)
+		}
+		fmt.Println("접속에 성공했습니다.")
+	}
+	defer res.Body.Close()
+	// fmt.Print(string(body))
 }
