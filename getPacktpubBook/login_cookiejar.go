22ac095bf7a02c43829ee2eb53f7321478de6c8f
forteleaf <forteleaf@live.com>
2016-06-29 12:44:19 +0900

    lunch time

--- getPacktpubBook/login_cookiejar.go
+++ getPacktpubBook/login_cookiejar.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strings"
 )
 
 const (
 	packtPubBaseURL string = "https://www.packtpub.com"
 	packtPubFreeURL string = "https://www.packtpub.com/packt/offers/free-learning"
-	// What You Need to Know about Docker - Free eBook
 )
 
 func main() {
-	var cookies []*http.Cookie
 	u, _ := url.Parse(packtPubBaseURL)
 	cookieJar, _ := cookiejar.New(nil)
+	var cookies []*http.Cookie
 	cookieJar.SetCookies(u, cookies)
 
 	userinfo := url.Values{}
@@ -24,21 +24,31 @@ func main() {
 	userinfo.Add("password", "shuria40")
 	userinfo.Add("op", "Login")
 
-	client := &http.Client{Jar: cookieJar}
-
-	req, _ := http.NewRequest("POST", "packtPubFreeURL", bytes.NewBufferString(userinfo.Encode()))
+	client := &http.Client{
+		Jar: cookieJar,
+	}
+	req, _ := http.NewRequest("POST", packtPubFreeURL, strings.NewReader(userinfo.Encode()))
+	// res, _ := client.PostForm(packtPubBaseURL, userinfo)
+	req.Header.Add("User-Agent", "Mozilla/5.0")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	//header 를 하니까 cookie 가 제대로 생성된다.
 
 	// body io.reader
-	client.Do(req)
-	client.PostForm("https://www.packtpub.com/freelearning-claim/19350/21478", userinfo)
-	// Getting Started with Electronic Projects
+	res, err := client.Do(req)
+	if err != nil {
+		panic(nil)
+	}
 	// https://www.packtpub.com/freelearning-claim/19350/21478
-	//resp, _ := client.Get("https://www.packtpub.com/freelearning-claim/25854/21478")
+	fmt.Println(res.Status)
+	resp, _ := client.Get("https://www.packtpub.com/freelearning-claim/19350/21478")
 
-	// client.Head("https://www.packtpub.com/freelearning-claim/25854/21478")
-	// client.PostForm("https://www.packtpub.com/freelearning-claim/25854/21478", userinfo)
+	// req1, _ := http.NewRequest("PUT", "https://www.packtpub.com/freelearning-claim/19350/21478", bytes.NewBufferString(userinfo.Encode()))
+	// req1.Header.Add("User-Agent", "Mozilla/5.0")
+	// req1.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	// resp, _ := client.Get("https://www.packtpub.com/freelearning-claim/19350/21478")
+	// resp, _ := client.Do(req1)
+	fmt.Println("============================" + resp.Status)
+
 	// res, _ := http.PostForm(packtPubBaseURL, userinfo)
 	//
 	// if res.StatusCode == 200 {
