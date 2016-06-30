f73076cdd97eb7101a078fc32275411e8c954cce
forteleaf <forteleaf@live.com>
2016-06-30 12:09:30 +0900

    login

--- getPacktpubBook/login_cookiejar.go
+++ getPacktpubBook/login_cookiejar.go
@@ -9,46 +9,33 @@ import (
 )
 
 const (
-	packtPubBaseURL string = "https://www.packtpub.com"
+	packtPubBaseURL string = "https://www.packtpub.com/"
 	packtPubFreeURL string = "https://www.packtpub.com/packt/offers/free-learning"
 )
 
 func main() {
-	u, _ := url.Parse(packtPubBaseURL)
 	cookieJar, _ := cookiejar.New(nil)
-	var cookies []*http.Cookie
-	cookieJar.SetCookies(u, cookies)
+	client := &http.Client{Jar: cookieJar}
 
 	userinfo := url.Values{}
 	userinfo.Add("email", "forteleaf@gmail.com")
 	userinfo.Add("password", "shuria40")
 	userinfo.Add("op", "Login")
+	userinfo.Add("form_id", "packt_user_login_form")
 
-	client := &http.Client{
-		Jar: cookieJar,
-	}
 	req, _ := http.NewRequest("POST", packtPubFreeURL, strings.NewReader(userinfo.Encode()))
-	// res, _ := client.PostForm(packtPubBaseURL, userinfo)
 	req.Header.Add("User-Agent", "Mozilla/5.0")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	//header 를 하니까 cookie 가 제대로 생성된다.
 
-	// body io.reader
-	res, err := client.Do(req)
-	if err != nil {
-		panic(nil)
-	}
-	// https://www.packtpub.com/freelearning-claim/19350/21478
-	fmt.Println(res.Status)
-	resp, _ := client.Get("https://www.packtpub.com/freelearning-claim/19350/21478")
+	res, _ := client.Do(req)
 
-	// req1, _ := http.NewRequest("PUT", "https://www.packtpub.com/freelearning-claim/19350/21478", bytes.NewBufferString(userinfo.Encode()))
-	// req1.Header.Add("User-Agent", "Mozilla/5.0")
-	// req1.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	// resp, _ := client.Get("https://www.packtpub.com/freelearning-claim/19350/21478")
-	// resp, _ := client.Do(req1)
-	fmt.Println("============================" + resp.Status)
-
+	fmt.Println(client.Jar)
+	res, _ = client.Get("https://www.packtpub.com/freelearning-claim/16784/21478")
+	// req, _ = http.NewRequest("GET", "https://www.packtpub.com/freelearning-claim/16784/21478", nil)
+	// res, _ = client.Do(req)
+	// https://www.packtpub.com/freelearning-claim/16784/21478
+	fmt.Println("===============" + res.Status)
+	fmt.Println(client.Jar)
 	// res, _ := http.PostForm(packtPubBaseURL, userinfo)
 	//
 	// if res.StatusCode == 200 {
