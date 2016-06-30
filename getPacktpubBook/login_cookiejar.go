c89a95464b7eb636ceb71832bcc2d2d2fd722a6d
forteleaf <forteleaf@live.com>
2016-06-30 12:14:12 +0900

    merge

--- getPacktpubBook/login_cookiejar.go
+++ getPacktpubBook/login_cookiejar.go
@@ -18,8 +18,8 @@ func main() {
 	client := &http.Client{Jar: cookieJar}
 
 	userinfo := url.Values{}
-	userinfo.Add("email", "forteleaf@gmail.com")
-	userinfo.Add("password", "shuria40")
+	userinfo.Add("email", "")
+	userinfo.Add("password", "")
 	userinfo.Add("op", "Login")
 	userinfo.Add("form_id", "packt_user_login_form")
 
